232cfbeffbea812546be4df7044a7d2bcad06fcc
CFSilveira <carloss.87@gmail.com>
2022-01-25 19:11:35 +0000

    done

--- js/index.js
+++ js/index.js
@@ -1,22 +1,42 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  
+  //DOM Elements
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  let newTotal = (price.innerHTML * quantity.value);
+  //document.querySelector('.subtotal span').innerHTML = newTotal  --> "simpler way"
+  // and bellow the same thing by creating a new variable
+  let newTotalSpan = product.querySelector('.subtotal span');
+  //here you change the specific part of the variable to the correct ammount
+  newTotalSpan.innerHTML = newTotal;
+  return newTotal
 
-  //... your code goes here
+
+  /* alternative solution:
+  updateSubtotal.innerText = Number((price.innerHTML)) * Number((quantity.value)); */
+
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+   /* const singleProduct = document.querySelector('.product');
+  updateSubtotal(singleProduct); */
   // end of test
+  const totalProducts = document.querySelectorAll('.product');
+  let totalPriceSum = 0;
+  for (let i = 0; i < totalProducts.length; i++) {
+    totalPriceSum += updateSubtotal(totalProducts[i]);
+  }
+  
+  console.log (totalPriceSum)
+  let totalPrice = document.querySelector('#total-value span');
+  //let totalPrice = document.getElementsByClassName ('#total-value span');
+  totalPrice.innerHTML = totalPriceSum;
 
-  // ITERATION 2
-  //... your code goes here
+  
 
   // ITERATION 3
   //... your code goes here
