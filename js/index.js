3a4593520b4e22098160d7eb837553110ee121e1
CFSilveira <carloss.87@gmail.com>
2022-01-26 11:36:07 +0000

    final version

--- js/index.js
+++ js/index.js
@@ -26,6 +26,7 @@ function calculateAll() {
   updateSubtotal(singleProduct); */
   // end of test
   const totalProducts = document.querySelectorAll('.product');
+  // OR   const totalProducts = document.getElementsByClassName('product');
   let totalPriceSum = 0;
   for (let i = 0; i < totalProducts.length; i++) {
     totalPriceSum += updateSubtotal(totalProducts[i]);
@@ -46,19 +47,61 @@ function calculateAll() {
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  console.log('The target in remove is:', target.parentNode.parentNode);
+  let rowToRemove = target.parentNode.parentNode;
+  let parent = rowToRemove.parentNode;
+  parent.removeChild(rowToRemove);
+
+  calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  let newProduct = document.querySelector('.create-product input');
+  let newPrice = document.querySelector('.create-product input[type="number');
+  //next line add 2 decimals to the right of the number (10.00, for example)
+  let fixedPrice = Number(newPrice.value).toFixed(2);
+  let newRow = document.createElement('tr');
+  newRow.innerHTML = `<td class="name">
+    <span>${newProduct.value}</span>
+  </td>
+  <td class="price">$<span>${fixedPrice}</span></td>
+  <td class="quantity">
+    <input type="number" value="0" min="0" placeholder="Quantity" />
+  </td>
+  <td class="subtotal">$<span>0</span></td>
+  <td class="action">
+    <button class="btn btn-remove">Remove</button>
+  </td>`
+  newRow.classList.add('product');
+  const parent = document.querySelector('#cart tbody');
+  parent.appendChild(newRow);
+
+  //making sure the new remove button work~
+  const removeBtn = newRow.querySelector('.btn-remove');
+  removeBtn.addEventListener('click', removeProduct);
+
+  //reseting the fields filled to create the new product
+  newProduct.value = '';
+  newPrice.value = 0;
+
 }
 
+//bellow is the code we want to run when the page loads (ie. add this functions to the buttons)
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+  
+  const removeButtons = document.getElementsByClassName('btn-remove');
+  for (let i = 0; i < removeButtons.length; i++) {
+    removeButtons[i].addEventListener('click', removeProduct);
+    // OR removeButtons[i].onClick, removeProduct;    
+  }
+
+  const createButton = document.getElementById('create')
+  createButton.addEventListener('click', createProduct);
+
 
   //... your code goes here
 });
